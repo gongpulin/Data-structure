b2633922c334045788229aad9dece3a6f89f181a
gongpulin <1255096785@qq.com>
2016-01-01 11:26:24 +0800

    Haffman的学习

---
+++ src/com/gpl/tree/haffman/test.java
@@ -0,0 +1,17 @@
+package com.gpl.tree.haffman;
+import org.junit.Test;
+public class test {
+
+	@Test
+	public void test_buildHaffmanTree(){
+		HaffmanTree ht=new HaffmanTree();
+		ht.ts.add(new Node("a",0.5f));
+		ht.ts.add(new Node("b",0.6f));
+		ht.ts.add(new Node("e",0.9f));
+		ht.ts.add(new Node("f",0.4f));
+		ht.ts.add(new Node("g",0.3f));
+		//ht.BuildHaffmaTree(ht.ts);
+		Node node1=ht.ts.pollLast();
+		System.out.println(node1.val+"gpl"+node1.freq);
+	}
+}
