a913471fd68d994fc0d3ffeb75ef7c621bae888f
gongpulin <1255096785@qq.com>
2016-03-21 22:46:43 +0800

    冒泡

---
+++ src/com/gpl/list/SqList/IList.java
@@ -0,0 +1,12 @@
+package com.gpl.list.SqList;
+
+public interface IList {
+	public void clear();
+	public boolean isEmpty();
+	public int length();
+	public Object get(int i) throws Exception;
+	public void insert(int i,Object x) throws Exception;
+	public void remove(int i) throws Exception;
+	public int indexOf(Object x);
+	public void display();
+}
--- src/com/gpl/sort/Bubblesort.java
+++ src/com/gpl/sort/Bubblesort.java
@@ -5,12 +5,12 @@ public class Bubblesort {
 		if(array==null)
 			return null;
 		int temp;
-		for(int i=0;i<array.length;i++){
-			for(int j=i+1;j<array.length;j++){
-				if(array[i]>array[j]){
-					temp=array[i];
-					array[i]=array[j];
-					array[j]=temp;
+		for(int i=1;i<array.length;i++){
+			for(int j=0;j<array.length-i;j++){
+				if(array[j]>array[j+1]){
+					temp=array[j];
+					array[j]=array[j+1];
+					array[j+1]=temp;
 				}
 			}
 		}
